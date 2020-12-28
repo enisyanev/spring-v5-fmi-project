fb4cb8b320cffeb9be225c5ffc8259f20877a63c
Petar Petrov <petar.petrov04@sap.com>
2020-12-28 17:41:51 +0200

    Add all user endpoints

--- src/main/java/com/project/spring/digitalwallet/web/UserController.java
+++ src/main/java/com/project/spring/digitalwallet/web/UserController.java
@@ -1,9 +1,16 @@
 package com.project.spring.digitalwallet.web;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +27,18 @@ public class UserController {
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+	@PostMapping
+	public User createUser(@Valid @RequestBody User user) {
+        User created = userService.addUser(user);
+        return created;
+    } 
+	@PutMapping
+	public void updateUser(@Valid @RequestBody User user) {
+		userService.updateUser(user);
+	}
+	@DeleteMapping
+	public User deleteUser(@RequestBody Map<String,String> usermap) {
+		return userService.deleteUser(usermap.get("username"));
+	}
 
 }

333e4067486ce5bff13ac5afccdb8e1d75714cd8
Petar Petrov <petar.petrov04@sap.com>
2021-01-31 17:21:34 +0200

    refactor code

--- src/main/java/com/project/spring/digitalwallet/dao/AccountRepository.java
+++ src/main/java/com/project/spring/digitalwallet/dao/AccountRepository.java
@@ -1,13 +1,17 @@
 package com.project.spring.digitalwallet.dao;
 
 import com.project.spring.digitalwallet.model.Account;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     List<Account> findByWalletId(Long walletId);
+
     Optional<Account> findByCurrencyAndWalletId(String currency, Long walletId);
+
     Optional<Account> findByIdAndWalletId(Long accountId, Long walletId);
 }
